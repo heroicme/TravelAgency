faa150208afd2d17dbdd43b24fde1d69891f619f
my <my@qq.com>
2018-06-10 22:52:18 +0800

    test change  1

--- src/main/java/my/test/MyTest.java
+++ src/main/java/my/test/MyTest.java
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger;
  */
 //wo change
     // change
+    //change1
 public class MyTest {
     public static void main(String[] args) {
         Logger log=Logger.getLogger(MyTest.class);
